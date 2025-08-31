97181382bcb7581929b25c9e8273c76a39fdb437
MdIftiabMahmudNabil <mdiftiabmahmudnabil0@gmail.com>
2025-08-31 15:53:28 +0600

    h

--- src/public/env.js.php
+++
@@ -1,7 +0,0 @@
-<?php 
-header('Content-Type: application/javascript'); 
-?>
-window.__SUPABASE__ = {
-  url: "<?= htmlspecialchars(getenv('SUPABASE_URL') ?: '', ENT_QUOTES) ?>",
-  anon: "<?= htmlspecialchars(getenv('SUPABASE_ANON_KEY') ?: '', ENT_QUOTES) ?>"
-};
